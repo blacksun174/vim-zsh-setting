e0194db0bf349eb7585e648f6a0c255dc296fb7d
blacksun174 <nhoangsocial@gmail.com>
2018-10-12 16:01:43 +0900

    word

---
+++ .shell.sh
@@ -0,0 +1,5 @@
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+# For example, we just use `~/.cache/dein` as installation directory
+sh ./installer.sh ~/.cache/dein
+
+mv vim-zsh-setting/.vimrc ~/
